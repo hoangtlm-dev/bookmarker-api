f722c55857b0687de8829355c89c709e44e67b49
Le Minh Hoang Tran <hoangtlm2410@gmail.com>
2024-06-15 16:50:26 +0000

    Set up book services

--- src/types/book.ts
+++ src/types/book.ts
@@ -2,6 +2,14 @@ export interface IBook {
 	name: string;
 	authors: string[];
 	image: string;
-	publishDate: string;
+	publishedDate: string;
+	description: string;
+}
+
+export interface IBookInput {
+	name: string;
+	authors: string;
+	image: string;
+	publishedDate: string;
 	description: string;
 }
