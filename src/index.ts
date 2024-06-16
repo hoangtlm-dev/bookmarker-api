aed1b59ed9374a6ca9880e5c29f0cf53f33ee28e
tranleminhhoang2410 <hoangtlm2410@gmail.com>
2024-06-16 15:00:16 +0700

    update port variable

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,9 @@ app.use('/', bookRoute());
 
 const server = http.createServer(app);
 
-server.listen(3000, () => {
-	console.log('Server is running on http://localhost:3000');
+// Start server
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
 });
